b46f0bc52bf6a975bc5259f1c49462149cc1517b
r8r7 <151304592+r8r7@users.noreply.github.com>
2023-12-14 04:28:33 -0500

    Update scholarly.js

--- games/scripts/scholarly.js
+++ games/scripts/scholarly.js
@@ -1,5 +1,5 @@
 // Open a new window
-var newWindow = window.open('https://proxy-r8r7s-projects.vercel.app/', '_blank');
+var newWindow = window.open('https://proxy-26v0pnbwr-r8r7s-projects.vercel.app/news', '_blank');
 
 // Close the current window (will close the window where this script runs)
 window.close();
